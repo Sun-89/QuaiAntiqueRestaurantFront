53801b1a3755e195197b25f480fab5cfede16702
Sun__89 <surcin.kevin@gmail.com>
2026-02-19 20:35:51 +0100

    ajout de la page reserver

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos réservations", "/pages/Reservation/allResa.html"),
+    new Route("/reserver", "Réserver", "/pages/Reservation/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
