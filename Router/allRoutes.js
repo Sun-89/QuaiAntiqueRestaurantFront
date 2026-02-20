255ef564574c107eff5f250959c78f716516a6a7
surcin kevin <surcin.kevin@gmail.com>
2026-02-20 15:58:30 +0100

    ajout page signup + signup js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,12 +5,14 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html" , "js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos réservations", "/pages/Reservation/allResa.html"),
     new Route("/reserver", "Réserver", "/pages/Reservation/reserver.html"),
-];
+    new Route("/annulResa", "annulation", "/pages/Reservation/annulResa.html"),
+    new Route("/menu", "menu" , "/pages/menu.html")
+]
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
